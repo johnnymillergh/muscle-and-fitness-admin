501b42e6c01c1c60b00d013262b89748e078546f
Johnny Miller (锺俊) <johnnysviva@outlook.com>
2020-02-21 08:57:32 +0800

    feat($muscle-and-fitness-admin): update favicon and logo
    M&F

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/logo.png" style="width: 256px">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
